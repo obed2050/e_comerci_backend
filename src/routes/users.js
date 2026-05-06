9184aed1b15b5e73b23dcdda0b086e343bf470d0
obed2050 <obedndayishimiye2050@gmail.com>
2026-05-06 14:10:11 +0200

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,9 +4,9 @@ import protect from "../middleware/auth.js";
 import authorize from "../middleware/authorize.js";
 const userRoutes=express.Router();
 
-userRoutes.get("/getAllUsers",protect,authorize("admin","seller"),getAllUsers);
-userRoutes.post("/createUser",protect,authorize("admin"),createUser);
-userRoutes.get("/getSingleUser/:id",protect,authorize("admin","seller"),getSingleUser);
+userRoutes.get("/getAllUsers",getAllUsers);
+userRoutes.post("/createUser",createUser);
+userRoutes.get("/getSingleUser/:id",getSingleUser);
 userRoutes.put("/updateUser/:id",protect,authorize("admin"),updateUser);
 userRoutes.delete("/deleteUser/:id",protect,authorize("admin"),deleteUser);
 
